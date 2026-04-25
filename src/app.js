60d7ac943a8331a5bcec4faa2c4686e589880729
IbraheemxDev <ibraheem.codes@gmail.com>
2026-04-25 22:24:00 +0500

    fix

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 import express from "express"
-const app=express()
+import authRouter from "./routes/auth.routes.js"  // ✅ Upar
 
-app.use(express.json({limit:"16kb"}))  //“Client JSON data bhejega, mujhe usay read karna hai.”
-app.use(express.urlencoded({extended :true,limit:"16kb"}))
+const app = express()
+
+app.use(express.json({limit:"16kb"}))
+app.use(express.urlencoded({extended:true, limit:"16kb"}))
 app.use(express.static("public"))
 // app.use(cookieParser())
-import authRouter from "./routes/auth.routes.js"
-app.use("/api/auth/register",authRouter)
 
+app.use("/api/auth", authRouter)  // ✅ Route bhi fix kiya
 
-export {app}
+export { app }
