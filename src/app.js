23cecb2c04e533fff675c65a8b9c85991fdda502
IbraheemxDev <ibraheem.codes@gmail.com>
2026-04-25 22:21:48 +0500

    fix

--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ app.use(express.json({limit:"16kb"}))  //“Client JSON data bhejega, mujhe usay
 app.use(express.urlencoded({extended :true,limit:"16kb"}))
 app.use(express.static("public"))
 // app.use(cookieParser())
-
+import authRouter from "./routes/auth.routes.js"
+app.use("/api/auth/register",authRouter)
 
 
 export {app}
